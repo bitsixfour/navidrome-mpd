36f1b1d34afd2d033cdfb88b48461e527afa3072
bitsixfour <tugirasles@gmail.com>
2026-05-14 19:50:12 -0500

    alsa

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,32 @@ use mpd::Client;
 use std::net::TcpStream;
 use clap::Parser;
 use std::error::Error;
-
+use anyhow::Error;
 
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    
     #[arg(short, long)]
-    name: String,
+    album: String,
 
-    /// Number of times to greet
+    #[arg(short,long)]
+    artist: String,
+
+    #[arg(short, long)]
+    song: String, 
+
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> anyhow::Result<()> {
     println!("starting mpd server....");
-    let args = Args::parse();
-    (())
+    println!("this is a test CLI so I can get Navidrome working");
+    let args: Args = Args::parse();
+    
+    Ok(())
 }
---
+++ target/debug/deps/mpdnavi-6f08444b2db1c5d1.d
@@ -0,0 +1,5 @@
+/home/will/git/mpdnavi/target/debug/deps/mpdnavi-6f08444b2db1c5d1.d: src/main.rs
+
+/home/will/git/mpdnavi/target/debug/deps/mpdnavi-6f08444b2db1c5d1: src/main.rs
+
+src/main.rs:
