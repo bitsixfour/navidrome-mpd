28ad720bad1e1e74512c963d9eafbdf9dd87af6e
bitsixfour <tugirasles@gmail.com>
2026-05-26 01:17:54 -0500

    struct

--- src/song.rs
+++ src/song.rs
@@ -19,30 +19,31 @@ pub struct MpdAlbum {
 impl FullAlbum {
     pub fn get(io: &str, resp: &NaviData ) -> Self {
         println!("finding data");   
-        let data: = resp.get(io).unwrap();
+        let data = resp.get(io).unwrap();
         let a_resp: Vec<MpdAlbum> = Vec::new();
-        (Self {
-            full_album: Vec<
-        })
-    }
-    fn to_text(&self) -> &str {
+        Self {
+            full_album: a_resp
+        }
 
+    }
+    async fn to_obj(&self) -> Vec<MpdAlbum> {
+        let vec: Vec<MpdAlbum> = Vec::new();
         println!("currentsong");
-        
-        let mut str: &str = 
-            "file: {} \n
-             Last-modified: {} \n
-             Format: {} \n
-             Artist: {} \n
-             AlbumArtist: {} \n
-             Title: {} \n
-             Album: {} \n
-             Track: {} \n
-             Time: {} \n
-             duration: {} \n
-             OK";
-        str
-        
+        let root = client
+            .get("http://192.168.1.20:8097/rest/getAlbumList2?u=nix&p=2008&v=1.16.1&c=test&f=json&type=alphabeticalByName&size=500")
+            .query(&[
+                ("f", "json"),
+                ("type", "alphabeticalByName"),
+                ("size", "500"),
+                ("offset", "0"),
+            ])
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<Root>()
+        .await?;
+
+        vec
         
     }
 
